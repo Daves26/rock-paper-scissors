1f42ccf004e77a155d52fa05797323d6da3c6a5b
Daves26 <juandavesga8@gmail.com>
2024-07-31 20:25:08 -0500

    Changed fonts and background colors

--- script.js
+++ script.js
@@ -5,18 +5,22 @@ let elements = ["rock", "paper", "scissors"];
 const results = document.querySelector(".results");
 const humanPoints = document.querySelector("#human_points");
 const computerPoints = document.querySelector("#computer_points");
+const body = document.querySelector("body");
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         if (roundsPlayed === 5) {
             if (humanScore > computerScore) {
+                body.style.backgroundColor = "#112716"
                 results.textContent = "=== Congratulations! You Win! ===";
                 return;
             } else if (computerScore > humanScore) {
+                body.style.backgroundColor = "#271111"
                 results.textContent = "=== You Lose ===";
                 return;
             } else {
+                body.style.backgroundColor = "#1a1e27"
                 results.textContent = "=== Tied! ===";
             }
         }
